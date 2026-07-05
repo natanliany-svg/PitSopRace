a53f5f216a4c3508aa342ce334460554bb806df5
Natan Liany <natanliany@gmail.com>
2026-07-05 12:09:20 +0300

    Executing the commands npm init -y and npm install readline-sync in the terminal In addition to inserting node_modules into the .gitignor folder
    In the race_service file, the data was retrieved from the server and the queue was managed with actions such as displaying the next vehicle/sending a radio message/searching by number and more...
    Also, a basic check that something is working in the main file
    |ביצוע הפקודות  npm init -y וגם npm install readline-sync בטרמינל
    בנוסף הכנסת node_modules לתיקיית .gitignor
    
    ובקובץ race_service בוצע שליפת הנתונים מהשרת וגם ניהל התור עם פעולות של הצגה של הרכב הבא/שליחת הודעת רדיו/חיפוש לפי מספר ועוד...
    כמו כן בדיקה בסיסית שמשהו עובד בקובץ ה main

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // קובץ הרצה של כל הנתונים 
+
+import { readRaceData, showQueue, showNextCar, sendRadio, searchByNum } from "./services/race_service.js"
+
+
+    const race = await readRaceData()
+
+
+    showQueue(race)
+    console.log("  ")
+
+
+
+
+
+    const nextCar = showNextCar(race.cars)
+    sendRadio(nextCar)
+    console.log("\n--- חיפוש מכונית לפי מספר  ---")
+
+
--- services/race_service.js
+++ services/race_service.js
@@ -1,1 +1,92 @@
 // קובץ לשליפת הנתונים מהרשרת וכן הצגה וניהול התור
+
+
+
+const apiUrl = "https://server-for-test-week-13.onrender.com/api/race"
+
+export async function readRaceData() {
+
+    try {
+        const res = await fetch(apiUrl)
+        if (!res.ok) {
+            throw new Error(`HTTP status: ${res.status}`)
+        }
+
+        const data = await res.json()
+        return data
+    } catch (err) {
+        console.log("שגיאה בהצגת הנתונים :", err.message)
+        return null
+    }
+}
+
+
+
+
+
+export function showQueue(race) {
+    const totalCars = race.cars.length
+    const doneList = race.cars.filter(c => c.status === "done")
+    const waitList = race.cars.filter(c => c.status === "wating")
+
+
+    console.log("========== PIT STOP QUEUE ==========")
+    console.log(`Race: ${race.raceName}`)
+    console.log(`Lap: ${race.currentLap} / ${race.totalLaps}`)
+    console.log(`Total cars in race: ${totalCars}`)
+    console.log(` Pit stops completed: ${doneList.length}`)
+    console.log("=====");
+    
+    console.log("Cars waiting for pit stop:")
+    waitList.forEach(c => {
+        console.log(`- Car #${c.carNumber} | Driver: ${c.driverName}`)
+    })
+}
+
+
+export function showNextCar(cars) {
+    const nextCar = cars.find(c => c.status === "waiting")
+    if (nextCar) {
+        console.log("Next car the enter the pit:")
+        console.log(`>> Car #${nextCar.carNumber} | Driver: ${nextCar.driverName}`)
+        console.log("==================================")
+    }
+    return nextCar
+}
+
+
+
+export function sendRadio(car) {
+    if (car) {
+        console.log(`Radio mesage to car #${car.carNumber}: "Box  box  box, ${car.driverName}, pit this lap!"`)
+    }
+}
+
+export function searchByNum(cars, num) {
+    const found = cars.find(c => c.carNumber === num)
+    return found
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
